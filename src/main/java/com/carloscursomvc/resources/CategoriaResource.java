0c728860e4a597fad8704b61cc1c712ce304761d
CarlosSantos39 <novasintoniasp@gmail.com>
2021-10-23 13:36:42 -0300

    Testando Class de dominio

--- src/main/java/com/carloscursomvc/resources/CategoriaResource.java
+++ src/main/java/com/carloscursomvc/resources/CategoriaResource.java
@@ -1,21 +1,30 @@
 package com.carloscursomvc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.carloscursomvc.domain.Categoria;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
 
 	@RequestMapping(method = RequestMethod.GET)
-	public String listar() {
-		return "Rest esta Funcionando!";
+	public List<Categoria> listar() {
+
+		Categoria cat1 = new Categoria(1, "Informatica");
+		Categoria cat2 = new Categoria(2, "Escritorio");
+
+		List<Categoria> list = new ArrayList<>();
+		list.add(cat1);
+		list.add(cat2);
+
+		return list;
 
 	}
 
-	@RequestMapping(method = RequestMethod.POST)
-	public String usar() {
-		return "Rest esta Funcionando!";
-	}
 }
