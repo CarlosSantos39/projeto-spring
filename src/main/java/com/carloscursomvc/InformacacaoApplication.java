e15ee580c80c0db258136e905b50d46fb629a734
JoseCarlos <carlos.957990898@gmail.com>
2021-10-23 10:23:07 -0300

    Crição do Projeto spring

---
+++ src/main/java/com/carloscursomvc/InformacacaoApplication.java
@@ -0,0 +1,13 @@
+package com.carloscursomvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InformacacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InformacacaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/carloscursomvc/InformacacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carloscursomvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InformacacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
