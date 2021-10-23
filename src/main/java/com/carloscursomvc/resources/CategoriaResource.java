57acf70bf26b924cd53d188a639265d4b6f0f1b4
CarlosSantos39 <novasintoniasp@gmail.com>
2021-10-23 11:08:01 -0300

    Segundo comiete

--- src/main/java/com/carloscursomvc/resources/CategoriaResource.java
+++ src/main/java/com/carloscursomvc/resources/CategoriaResource.java
@@ -5,12 +5,17 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value="/categorias")
+@RequestMapping(value = "/categorias")
 public class CategoriaResource {
-	
-	@RequestMapping(method=RequestMethod.GET )
+
+	@RequestMapping(method = RequestMethod.GET)
 	public String listar() {
 		return "Rest esta Funcionando!";
 
+	}
+
+	@RequestMapping(method = RequestMethod.POST)
+	public String usar() {
+		return "Rest esta Funcionando!";
 	}
 }
