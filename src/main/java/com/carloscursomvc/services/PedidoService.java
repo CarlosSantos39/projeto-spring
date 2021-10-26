f8f6dfcb0c9c9b61a09d9dff21b730ebe8f06d72
CarlosSantos39 <novasintoniasp@gmail.com>
2021-10-26 15:09:34 -0300

    Atualizacao utilizando somenete JsonIgnor

---
+++ src/main/java/com/carloscursomvc/services/PedidoService.java
@@ -0,0 +1,23 @@
+package com.carloscursomvc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.carloscursomvc.domain.Pedido;
+import com.carloscursomvc.repositories.PedidoRepository;
+import com.carloscursomvc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PedidoService {
+
+	@Autowired
+	private PedidoRepository repo;
+
+	public Pedido find(Integer id) {
+		Optional<Pedido> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName()));
+	}
+}
