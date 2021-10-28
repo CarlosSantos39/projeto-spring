c2140e287290a9cac8d18d77ac2e304a9f28b1b8
CarlosSantos39 <novasintoniasp@gmail.com>
2021-10-28 12:44:29 -0300

    Paginacao com paramentro opcionais na requisicao

--- src/main/java/com/carloscursomvc/services/CategoriaService.java
+++ src/main/java/com/carloscursomvc/services/CategoriaService.java
@@ -5,12 +5,16 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.carloscursomvc.domain.Categoria;
 import com.carloscursomvc.repositories.CategoriaRepository;
 import com.carloscursomvc.services.exceptions.DataInterityException;
 import com.carloscursomvc.services.exceptions.ObjectNotFoundException;
+
 
 @Service
 public class CategoriaService {
@@ -46,5 +50,9 @@ public class CategoriaService {
 	}
 	public List<Categoria> findAll(){
 		return repo.findAll();
+	}
+	public Page<Categoria> finPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest);
 	}
 }
