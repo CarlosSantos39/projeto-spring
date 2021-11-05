a9508f8279bc84222e60a0a38d881794b67d73ae
CarlosSantos39 <novasintoniasp@gmail.com>
2021-11-05 13:10:51 -0300

    Enviando email com STPGOOGLE

--- src/main/java/com/carloscursomvc/config/DevConfig.java
+++ src/main/java/com/carloscursomvc/config/DevConfig.java
@@ -9,6 +9,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import com.carloscursomvc.services.DBService;
+import com.carloscursomvc.services.EmailService;
+import com.carloscursomvc.services.SmtpEmailService;
 
 @Configuration
 @Profile("dev")
@@ -30,5 +32,9 @@ public class DevConfig {
 		dbService.instantiateTestDatabase();
 		return true;
 	}
+    @Bean
+    public EmailService emailService() {
+    	return new SmtpEmailService();
+    }
 
 }
